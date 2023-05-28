e47a083e9fcb8c38993ce09bd1e9779f189e0ddd
amalujaison <94656761+amalujaison@users.noreply.github.com>
2023-05-28 19:14:26 +0530

    Delete elearningapp directory

--- elearningapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib import admin
-from django.http import HttpResponse
-from elearnapp.models import Course, Mentor, Quiz, payment
-from elearnapp.models import Account
-
-admin.site.register(Account)
